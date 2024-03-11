b83fed39b56e37800d9f70b6f62f4249a469573c
SharpLad <143443228+yrezvani@users.noreply.github.com>
2024-03-11 23:15:51 +0000

    Remove redundant code from controller module

--- src/controller.js
+++ src/controller.js
@@ -5,57 +5,14 @@ const controller = function () {
     let playerGameboard = createGameboard();
     let aiGameboard = createGameboard();
 
-    let huPlayer = player();
-    let aiPlayer = player();
-    let playerTurn = true;
-
-    const playerAttack = function (x, y) {
-        const coordStr = `${x.toString()},${y.toString()}`;
-        if (!playerGameboard.prevMoves.includes(coordStr)) {
-            playerGameboard.prevMoves.push(coordStr);
-            aiGameboard.receiveAttack(x, y);
-            if (checkWin(huPlayer)) {
-                console.log('Player wins');
-            } else playerTurn = !playerTurn;
-        } else {
-            console.log('Illegal move! Try again.');
-        }
-    };
-
-    const aiAttack = function (x, y) {
-        const attCoord = aiPlayer.generateRandCoordinates();
-        playerGameboard.receiveAttack(attCoord.x, attCoord.y);
-
-        if (checkWin(aiPlayer)) {
-            console.log('AI Wins!');
-        } else playerTurn = !playerTurn;
-    };
-
-    const checkWin = function (player) {
-        if (player === huPlayer) {
-            return aiGameboard.allSunk();
-        } else {
-            return playerGameboard.allSunk();
-        }
-    };
-
     function resetGame() {
         playerGameboard = createGameboard();
         aiGameboard = createGameboard();
-        huPlayer = player();
-        aiPlayer = player();
-        playerTurn = true;
     }
 
     return {
         playerGameboard,
         aiGameboard,
-        huPlayer,
-        aiPlayer,
-        playerTurn,
-        playerAttack,
-        aiAttack,
-        checkWin,
         resetGame,
     };
 };
